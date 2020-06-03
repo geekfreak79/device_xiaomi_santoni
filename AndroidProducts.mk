dfd31f238bb46f5c48ba488c307bd738a50d3a47
Ahoy <geekfreak.ahoy123@gmail.com>
2020-06-03 02:12:57 +0000

    first commit

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_santoni.mk
+    $(LOCAL_DIR)/aim_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
+    aim_santoni-userdebug \
+    aim_santoni-user \
+    aim_santoni-eng
